263cf38dbd151f2b47f27dd6a0b9519d064a1c9b
BelloCM <kekanacm@gmail.com>
2024-07-31 22:26:40 +0200

    added javascript apikey and instructions

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-function generatePoem(event){
+function displayPoem(response){
 
-    event.preventDefault()
-    
-    new Typewriter("#poem", {
-      strings:"The love is not love without action",
-      autoStart: true,
-      Delay: 1,
-      cursor:""
-    });
+  
+  new Typewriter("#poem", {
+    strings:response.data.answer,
+    autoStart: true,
+    Delay: 1,
+    cursor:""
+  });
 }
 
+function generatePoem(event){
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "9b2ab0b45ef9f3fd42t449a3oba19480";
+  let prompt = `User instructions: Generate love poems ${"instructionsInput"}`;
+  let context =
+    "you are the best love poem writer and your mission is to generate 5 lines of poem and separate the lines using this element <br/> and make sure that you follow user instructions, sign the buttom of the poem  with <strong>'Bello'<strong/> element..";
+    let apiUrl =
+    `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
+  axios.get(apiUrl).then(displayPoem);
+};
 
 let poemFormElement = document.querySelector("#poem-generator-foem");
-poemFormElement.addEventListener("submit", generatePoem )
-
+poemFormElement.addEventListener("submit", generatePoem );
